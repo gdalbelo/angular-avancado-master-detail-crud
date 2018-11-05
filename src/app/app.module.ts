0bf5ec3e36fd12df8d40db9f72e2ec7f164dc14e
Gabriel Santos Dal Belo <gdalbelo@vayon.com.br>
2018-11-05 16:25:45 -0200

    adding categories module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryFormComponent } from './pages/categories/category-form/category-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryFormComponent
   ],
   imports: [
     BrowserModule,
